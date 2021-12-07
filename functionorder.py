487e0b4c03999b761019e041d78e9590518a55e8
AnyBuzzin <35739315+AnyBuzzin@users.noreply.github.com>
2021-12-07 08:06:57 +0000

    House Keeping

--- functionorder.py
+++ functionorder.py
@@ -5,9 +5,11 @@ import time
 
 
 db = input("Enter the Exact file path to The DataBase  ")
+db  = "C:\Windows\System32\lillymay/Carmex.sqlite3"
 while True:
     start = time.perf_counter()
-    Encoding.main(db,DDscrape.main(db))
+    DDscrape.main(db)
+    Encoding.main(db)
     finish = time.perf_counter()
     print("Duration" + str(start-finish))
 
